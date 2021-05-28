1d1e000b5fe28e427250615886a7b9c032821281
bulloc56 <kevinbullock3@gmail.com>
2021-05-28 01:28:23 -0400

    added chat widget
    fixed email/phone lookup
    added chat widget

--- public/functions.js
+++ public/functions.js
@@ -15,23 +15,137 @@ const database = firebase.firestore();
 const usersCollection = database.collection('users');
 
 window.addEventListener("load", function () {
-    addInfoTest();
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    if (urlParams.has('email')){
+        getCustomerDataByEmail(urlParams.get('email'));
+    }
+    else if (urlParams.has('phone')){
+        getCustomerDataByPhone(urlParams.get('phone'));
+    }
+    else{
+        //do nothing
+    }
+    
 })
 
-function addInfoTest(){
-    var info;
-    usersCollection.where("email", "==", "kevinbullock3@gmail.com").get()
+function getCustomerDataByEmail(address){
+    var containerUserElement = document.createElement('div');
+    containerUserElement.id = 'Customer';
+    document.body.appendChild(containerUserElement);
+    var custName = document.createElement('div');
+    custName.class = "CustomerName";
+    containerUserElement.appendChild(custName);
+    var custMail = document.createElement('div');
+    custMail.class = 'CustomerEmail';
+    containerUserElement.appendChild(custMail);
+    var custPhone = document.createElement('div');
+    custPhone.class = 'CustomerPhone';
+    containerUserElement.appendChild(custPhone);
+
+
+    usersCollection.where("email", "==", address).get()
     .then((querySnapshot) => {
         querySnapshot.forEach((doc) => {
             console.log(doc.id, " => ", doc.data());
-            info = doc.data();
-            var divElement = document.createElement('div');
-            divElement.innerHTML = info.email+"\n"+info.phone+"\n"+info.firstName+"\n"+info.lastName;
-            document.body.appendChild(divElement);
+            var info = doc.data();
+            custName.innerText = info.firstName +" "+info.lastName;
+            custMail.innerText = info.email;
+            custPhone.innerText = info.phone;
+
+            doc.ref.collection("Order History").get().then(function (orderQuery){
+                orderQuery.forEach((order) => {
+                    console.log(order.id, "=>", order.data());
+                    var temp = order.data();
+                    createOrderElements(order.id, temp.Item,temp.Rating,temp.Recommended, temp.Vendor, temp.Price);
+                })
+            }).catch((error) => {
+                console.log("Error getting documents: ", error);
+            });
         });
     })
     .catch((error) => {
         console.log("Error getting documents: ", error);
     });
     
+}
+
+function getCustomerDataByPhone(number){
+    var containerUserElement = document.createElement('div');
+    containerUserElement.id = 'Customer';
+    document.body.appendChild(containerUserElement);
+    var custName = document.createElement('div');
+    custName.class = "CustomerName";
+    containerUserElement.appendChild(custName);
+    var custMail = document.createElement('div');
+    custMail.class = 'CustomerEmail';
+    containerUserElement.appendChild(custMail);
+    var custPhone = document.createElement('div');
+    custPhone.class = 'CustomerPhone';
+    containerUserElement.appendChild(custPhone);
+
+
+    usersCollection.where("phone", "==", number).get()
+    .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            console.log(doc.id, " => ", doc.data());
+            var info = doc.data();
+            custName.innerText = info.firstName +" "+info.lastName;
+            custMail.innerText = info.email;
+            custPhone.innerText = info.phone;
+
+            doc.ref.collection("Order History").get().then(function (orderQuery){
+                orderQuery.forEach((order) => {
+                    console.log(order.id, "=>", order.data());
+                    var temp = order.data();
+                    createOrderElements(order.id, temp.Item,temp.Rating,temp.Recommended, temp.Vendor, temp.Price);
+                })
+            }).catch((error) => {
+                console.log("Error getting documents: ", error);
+            });
+        });
+    })
+    .catch((error) => {
+        console.log("Error getting documents: ", error);
+    });
+    
+}
+
+
+function createOrderElements(id, item, rating, recommended, vendor, price){
+    var containerOrdersElement = document.createElement('div');
+    containerOrdersElement.id = 'Orders';
+    var orderList = document.createElement('div');
+    containerOrdersElement.appendChild(orderList);
+    document.body.appendChild(containerOrdersElement);
+    var newOrder = document.createElement('li');
+    newOrder.class = "Order";
+    orderList.appendChild(newOrder);
+    var orderElements = document.createElement('ul');
+    orderElements.class = "OrderElements";
+    newOrder.appendChild(orderElements);
+    var orderElement0 = document.createElement('div');
+    orderElement0.class = "OrderID";
+    orderElement0.innerText = "Transaction ID : "+id;
+    orderElements.appendChild(orderElement0);
+    var orderElement1 = document.createElement('li');
+    orderElement1.class = "OrderItem";
+    orderElement1.innerText = "Item Name : "+item;
+    orderElements.appendChild(orderElement1);
+    var orderElement2 = document.createElement('li');
+    orderElement2.class = "OrderPrice";
+    orderElement2.innerText = "Price : $"+price;
+    orderElements.appendChild(orderElement2);
+    var orderElement3 = document.createElement('li');
+    orderElement3.class = "OrderRating";
+    orderElement3.innerText = "Rating : "+rating;
+    orderElements.appendChild(orderElement3);
+    var orderElement4 = document.createElement('li');
+    orderElement4.class = "OrderRecommended";
+    orderElement4.innerText = "Recommended : "+recommended;
+    orderElements.appendChild(orderElement4);
+    var orderElement5 = document.createElement('li');
+    orderElement5.class = "OrderVendor";
+    orderElement5.innerText = "Vendor : "+vendor;
+    orderElements.appendChild(orderElement5);
 }
