0cd85762f112cb250785f49b6f03c40903f2aed8
bulloc56 <kevinbullock3@gmail.com>
2021-05-30 17:29:19 -0400

    Unique pages for each
    pages and config files for each person

---
+++ public/WidgetConfig/James.js
@@ -0,0 +1,39 @@
+window._genesys = {
+    widgets: {
+      main: {
+                preload: ['webchat'],
+            },
+      webchat: {
+        chatButton: {
+            enabled: true, // (boolean) Enable/disable chat button on screen.
+          },
+        transport: {
+          type: 'purecloud-v2-sockets',
+          dataURL: 'https://api.usw2.pure.cloud',     // replace with API URL matching your region
+          deploymentKey : 'ec2458b4-bb3b-4ae9-b27f-fe3b9885890c',  // replace with your Deployment ID
+          orgGuid : '56ee736b-1261-4cd2-83d7-42b362223318',              // replace with your Organization ID
+          interactionData: {
+            routing: {
+              targetType: 'QUEUE',
+              targetAddress: 'BullockQueue',
+              priority: 2
+            }
+          }
+        },
+        userData: {
+          addressStreet: '64472 Brown Street',
+          addressCity: 'Lindgrenmouth',
+          addressPostalCode: '50163-2735',
+          addressState: 'FL',
+          phoneNumber: '1-916-892-2045 x293',
+          phoneType: 'Cell',
+          customerId: '59606',
+          // These fields should be provided via advanced configuration
+          // firstName: 'Praenomen',
+          // lastName: 'Gens',
+          // email: 'praenomen.gens@calidumlitterae.com',
+          // subject: 'Chat subject'
+        }
+      }
+    }
+  };
